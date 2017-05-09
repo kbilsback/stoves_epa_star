678290abb43664b868c5956b5290014da7f33289
kbilsback <kbilsbac@rams.colostate.edu>
2017-05-09 15:56:25 -0600

    add boxplot function

--- r_scripts/R_plots.R
+++ r_scripts/R_plots.R
@@ -19,3 +19,21 @@ plot_timeseries_qc <- function(df) {
 
 }
 #________________________________________________________
+
+#________________________________________________________
+# plot timeseries data by qc color
+# takes a data fram with columns labeled var and value with variable and value to plot
+boxplot_timeseries_qc <- function(df) {
+  
+  p <- ggplot(df, aes(value, color = first(qc))) +
+    geom_line() +
+    facet_wrap(~hh_id, ncol = 1, scales = "free") +
+    theme_minimal() +
+    theme(legend.position = "top") +
+    ylab(paste(first(var), first(units)))
+  # return plot
+  return(p)
+  
+}
+#________________________________________________________
+
