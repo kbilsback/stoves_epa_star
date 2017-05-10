338edb30fde6c50249c489986415685a01049124
kbilsback <kbilsbac@rams.colostate.edu>
2017-05-10 17:22:34 -0600

    update timeseries plot

--- r_scripts/plots.r
+++ r_scripts/plots.r
@@ -4,16 +4,17 @@
 #________________________________________________________
 
 #________________________________________________________
-# plot timeseries data by qc color
+# plot timeseries data (default by qc color)
 # takes a data frame and a column name of variable to plot
-plot_timeseries_qc <- function(df, var_name) {
+field_timeseries_plot <- function(df, y_var, color_var = "qc", x_var = "datetime", facet_var = "hh_id", y_units = "units") {
   
-  p <- ggplot(df, aes(datetime, temp, color = qc)) +
+  p <- ggplot(df, aes_string(y = y_var, x = x_var, color = color_var)) +
          geom_line() +
-         #facet_wrap(~hh_id, ncol = 1, scales = "free") +
+         facet_wrap(~df[[facet_var]], ncol = 1, scales = "free") +
          theme_minimal() +
          theme(legend.position = "top") +
-         ylab(first(units))
+         ylab(paste0(y_var, " (", df[[1, y_units]], ")")) +
+         xlab(x_var)
 
   # return plot
   return(p)
@@ -24,21 +25,21 @@ plot_timeseries_qc <- function(df, var_name) {
 #________________________________________________________
 # plot timeseries data by a fill color
 # takes a data frame and a column name of variable to plot
-boxplot_timeseries <- function(df, y_var, fill_var = "qc", x_var = "hh_id", y_units = "units", x_units = "") {
+field_boxplot <- function(df, y_var, fill_var = "qc", x_var = "hh_id", y_units = "units") {
 
   p <-   ggplot(df, aes_string(y = y_var, x = x_var, fill = fill_var)) +
           geom_boxplot() +
+          scale_fill_discrete(drop = FALSE) +
           theme_minimal() +
           theme(legend.position = "top") +
-          ylab(paste0(y_var, df[[1, y_units]])) +
-          #xlab(df)
+          ylab(paste0(y_var, " (", df[[1, y_units]], ")")) +
+          xlab(x_var)
 
   # return plot
   return(p)
   
 }
 #________________________________________________________
-
 
 #________________________________________________________________
 # boxplot with outlier labels
