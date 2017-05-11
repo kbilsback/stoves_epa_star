dff83318f43284e2487df80345e29e3c7161318d
kbilsback <kbilsbac@rams.colostate.edu>
2017-05-11 10:00:10 -0600

    add sums data reader

--- r_scripts/load_data.r
+++ r_scripts/load_data.r
@@ -85,32 +85,24 @@ load_temp_file <- function(file){
 # Load sums file
 # file <- "../data/sums/XXX.csv"
 load_sums_file <- function(file){
+
+  data <- read.csv(file, fill = TRUE, stringsAsFactors = FALSE, header = FALSE, skip = 20, col.names = c("datetime", "units", "stove_temp"))
+
+  data <- dplyr::mutate(data, logger_id = gsub(".*: ", "", read.csv(file, stringsAsFactors = FALSE, nrows = 1, col.names = "id")))
   
-  data <- read.csv(file, fill = TRUE, stringsAsFactors = FALSE, col.names = c("date", "time", "temp", "logger_id"))
-  
-  data <- dplyr::mutate(data, logger_id = data[2,3]) %>%
-    dplyr::filter(grepl("^[0-9]", date))
-  
-  if(nchar(data$time[1]) == 10){
-    data <- dplyr::mutate(data, time = as.character(strftime(strptime(time, "%I:%M:%S %p"), "%H:%M:%S")))
-  } 
-  if(substring(data$date[1], 1, 1) == "0"){
-    data <- dplyr::mutate(data, date = as.character(as.Date(data$date, "%d/%m/%Y"))) 
-  }else{
-    data <- dplyr::mutate(data, date = as.character(as.Date(date, "%m/%d/%y")))
+  if(substring(data$date[1], 6, 7) == "00"){
+    data <- dplyr::mutate(data, datetime = gsub("00", "16", datetime))
   }
+
+  data <- dplyr::mutate(data, datetime = as.POSIXct(strftime(strptime(datetime,
+                                                                      "%d/%m/%y %I:%M:%S %p")), "%Y-%m-%d %H:%M:%S")) %>%
+          dplyr::mutate(date = as.POSIXct(format(datetime, "%Y-%m-%d"))) %>%
+          dplyr::mutate(time = as.character(datetime)) %>%
+          dplyr::mutate(time = as.numeric(substr(time, 12, 13)) * 60 * 60 + 
+                               as.numeric(substr(time, 15, 16)) * 60 +
+                               as.numeric(substr(time, 18, 19)))
   
-  data <- dplyr::mutate(data, datetime = as.POSIXct(paste(data$date, data$time), 
-                                                    format = "%Y-%m-%d %H:%M:%S"))
-  
-  data <- dplyr::mutate(data, date = as.POSIXct(date))
-  
-  data <- dplyr::mutate(data, time = as.character(as.POSIXct(strptime(time, "%H:%M:%S")))) %>%
-    dplyr::mutate(time = as.numeric(substr(time, 12, 13)) * 60 * 60 + 
-                    as.numeric(substr(time, 15, 16)) * 60 +
-                    as.numeric(substr(time, 18, 19)))
-  
-  data <- dplyr::mutate(data, temp = as.numeric(temp))
+  data <- dplyr::mutate(data, stove_temp = as.numeric(stove_temp))
   
   # return
   return(data)
