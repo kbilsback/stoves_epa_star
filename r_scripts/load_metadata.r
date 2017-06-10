3b3c680b50cb7bffd46d2e0fb53eea11872dcf99
kbilsback <kbilsbac@rams.colostate.edu>
2017-06-10 13:20:00 -0600

    update file readers

--- r_scripts/load_metadata.r
+++ r_scripts/load_metadata.r
@@ -53,44 +53,45 @@ load_field_meta <- function(){
 #________________________________________________________
 
 #________________________________________________________
-# load field filter meta data 
-load_field_filter_meta <- function(){
+# load field flow rates
+load_field_flows <- function(){
 
-  file <- list.files("../data/field/meta", "field_grav_meta.csv", full.names = TRUE)
+  return(read_csv("../data/field/meta/inst_flows.csv",
+                  col_names = TRUE,
+                  col_types = cols(hh_id = col_character(),
+                                   inst = col_factor(levels = c("filter_1", "filter_2",
+                                                                   "bypass", "aqe", "microaeth",
+                                                                   "smps")),
+                                   pre_flow_setting = col_double(),
+                                   pre_flow_1 = col_double(),
+                                   pre_flow_2 = col_double(),
+                                   pre_flow_3 = col_double(),
+                                   post_flow_setting = col_double(),
+                                   post_flow_1 = col_double(),
+                                   post_flow_2 = col_double(),
+                                   post_flow_3 = col_double(),
+                                   notes = col_character()),
+                  na = c("", "NA")))
 
-  print(file)
-
-  data <- read.csv(file, header = TRUE, stringsAsFactors = FALSE, fill = TRUE, na.strings = c("", "NA"))
-  
-  data <- dplyr::mutate(data, date = as.character(as.Date(date, "%m/%d/%y"))) %>%
-          dplyr::mutate(date = as.POSIXct(date, tz = "Asia/Calcutta"))
-  # will need to add additional exceptions for other field sites (use replace function)
-
-  data <- dplyr::mutate(data, hh_id = as.factor(hh_id),
-                              cart_type = as.factor(cart_type),
-                              filter_type = as.factor(filter_type))
-
-  # return 
-  return(data)
-  
 }
 #________________________________________________________
 
 #________________________________________________________
-# load field flow rates
-load_field_flows <- function(){
+# load field filter meta data 
+load_field_filter_meta <- function(){
 
-  file <- list.files("../data/field/meta", "inst_flows.csv", full.names = TRUE)
-
-  print(file)
-  
-  flows <- read.csv(file, header = TRUE, stringsAsFactors = FALSE, fill = TRUE, na.strings = c("", "NA"))
-  
-  data <- dplyr::mutate(data, hh_id = as.factor(hh_id),
-                        inst = as.factor(inst))
-
-  # return 
-  return(data)
+  return(read_csv("../data/field/meta/field_grav_meta.csv",
+                  col_names = TRUE, 
+                  col_types = cols(
+                                   date = col_date(format = "%m/%d/%y"),
+                                   hh_id = col_character(),
+                                   filter_type = col_character(),
+                                   cart_id = col_character(),
+                                   quartz_id = col_character(),
+                                   teflon_id = col_character(),
+                                   cart_type = col_factor(levels = c("single", "double")),
+                                   notes = col_character()),
+                  na = c("", "NA")))
 
 }
 #________________________________________________________
@@ -101,14 +102,14 @@ load_field_temp_meta <- function(){
 
   return(read_csv("../data/field/meta/field_temp_meta.csv",
                   col_names = TRUE,
-                  col_types = cols(field_site = 
-                                     col_factor(levels = c("india", "uganda",
-                                                           "china", "honduras")),
+                  col_types = cols(field_site = col_factor(levels = c("india", "uganda",
+                                                                      "china", "honduras")),
                                    hh_id = col_character(),
                                    alt_id = col_character(),
                                    logger_type = col_factor(levels = c("omega", "sums")),
                                    logger_id = col_character(),
-                                   notes = col_character())))
+                                   notes = col_character()),
+                  na = c("", "NA")))
 
 }
 #________________________________________________________
@@ -122,7 +123,8 @@ load_field_notes <- function(){
                   col_types = cols(hh_id = col_character(),
                                    inst = col_character(),
                                    notes = col_character(),
-                                   qc = col_character()))) 
+                                   qc = col_character()),
+                  na = c("", "NA"))) 
 
 }
 #________________________________________________________   
