d886034b306b26f69dd5cf62c7438a1d9ba7e794
kbilsback <kbilsbac@rams.colostate.edu>
2017-06-12 14:17:29 -0600

    get rid of return statements

--- r_scripts/load_data.r
+++ r_scripts/load_data.r
@@ -10,19 +10,20 @@ load_field_temp <- function(){
   return(lapply(list.files("../data/field/temp",
                            pattern = "429.csv",
                            full.names = TRUE),
-                function(x) readr::read_csv(x, skip = 20,
+                function(x) readr::read_csv(x, skip = 7,
                                             col_names = c("date", "time",
-                                                          "stove_temp"),
+                                                          "temp"),
                                             col_types = cols(
-                                              datetime = col_character(),
-                                              units = col_character(),
-                                              stove_temp = col_double()),
-                                            na = c("", "NA"))) %>%
+                                              date = col_character(),
+                                              time = col_time(format = "%I:%M:%S %p"),
+                                              temp = col_double()),
+                                            na = c("", "NA")) %>%
                   dplyr::mutate(logger_id = 
-                                  gsub(".*: ", "", 
-                                       readr::read_csv(x, skip = 1, n_max = 1,
-                                                       col_names = "id",
+                                  gsub("Serial Number: ", "", 
+                                       readr::read_csv(x, skip = 2, n_max = 1,
                                                        col_types = cols(
+                                                         "_",
+                                                         "_",
                                                          id = col_character()))))) %>%
            dplyr::bind_rows() %>%
            dplyr::mutate(datetime = as.POSIXct(gsub("00", "16", datetime), 
@@ -71,30 +72,29 @@ load_field_temp <- function(){
 # Load sums file
 load_field_sums <- function(){
 
-  return(lapply(list.files("../data/field/sums",
-                           pattern = ".csv",
-                           full.names = TRUE),
-                function(x) readr::read_csv(x, skip = 20,
-                                             col_names = c("datetime", "units",
-                                                           "stove_temp"),
-                                             col_types = cols(
-                                               datetime = col_character(),
-                                               units = col_character(),
-                                               stove_temp = col_double()),
-                                             na = c("", "NA")) %>%
-                  dplyr::mutate(logger_id = 
-                                  gsub(".*: ", "", 
-                                       readr::read_csv(x, skip = 1, n_max = 1,
-                                                       col_names = "id",
-                                                       col_types = cols(
-                                                         id = col_character()))))) %>%
-           dplyr::bind_rows() %>%
-           dplyr::mutate(datetime = as.POSIXct(gsub("00", "16", datetime), 
-                                               format = "%d/%m/%y %I:%M:%S %p"),
-                         date = as.Date(datetime),
-                         time = as.numeric(substr(datetime, 12, 13)) * 60 * 60 +
-                                as.numeric(substr(datetime, 15, 16)) * 60 +
-                                as.numeric(substr(datetime, 18, 19))))
+  lapply(list.files("../data/field/sums",
+                    pattern = ".csv",
+                    full.names = TRUE),
+         function(x) readr::read_csv(x, skip = 20,
+                                     col_names = c("datetime", "units",
+                                                   "stove_temp"),
+                                     col_types = cols(
+                                       datetime = col_character(),
+                                       units = col_character(),
+                                       stove_temp = col_double()),
+                                     na = c("", "NA")) %>%
+           dplyr::mutate(logger_id = gsub(".*: ", "", 
+                                          readr::read_csv(x, skip = 1, n_max = 1,
+                                                          col_names = "id",
+                                                          col_types = cols(
+                                                            id = col_character()))))) %>%
+    dplyr::bind_rows() %>%
+    dplyr::mutate(datetime = as.POSIXct(gsub("00", "16", datetime), 
+                                        format = "%d/%m/%y %I:%M:%S %p"),
+                  date = as.Date(datetime),
+                  time = as.numeric(substr(datetime, 12, 13)) * 60 * 60 +
+                         as.numeric(substr(datetime, 15, 16)) * 60 +
+                         as.numeric(substr(datetime, 18, 19)))
 }
 #________________________________________________________
 
@@ -102,25 +102,25 @@ load_field_sums <- function(){
 # Load grav file
 load_field_grav <- function(){
 
-  return(lapply(list.files("../data/field/grav",
-                           pattern = "grav.csv",
-                           full.names = TRUE),
-                function(x) readr::read_csv(x, col_names = TRUE,
-                                            col_types = cols(
-                                              .default = col_double(),
-                                              id = col_character(),
-                                              pre_date = col_character(),
-                                              blank_id = col_character(),
-                                              post_date = col_date(format = "%d/%m/%Y"),
-                                              post_pressure = col_character(),
-                                              post_blank_id = col_character(),
-                                              notes = col_character()),
-                                            na = c("", "NA"))) %>% 
-           dplyr::bind_rows() %>%
-           dplyr::mutate(pre_date = as.Date(ifelse(grepl("^IN[A-Z]", pre_date),
-                                           as.Date(pre_date, format = "%d/%m/%y"),
-                                           as.Date(pre_date, format = "%m/%d/%y")),
-                                           origin = "1970-01-01")))
+  lapply(list.files("../data/field/grav",
+                    pattern = "grav.csv",
+                    full.names = TRUE),
+         function(x) readr::read_csv(x, col_names = TRUE,
+                                     col_types = cols(
+                                       .default = col_double(),
+                                       id = col_character(),
+                                       pre_date = col_character(),
+                                       blank_id = col_character(),
+                                       post_date = col_date(format = "%d/%m/%Y"),
+                                       post_pressure = col_character(),
+                                       post_blank_id = col_character(),
+                                       notes = col_character()),
+                                     na = c("", "NA"))) %>% 
+    dplyr::bind_rows() %>%
+    dplyr::mutate(pre_date = as.Date(ifelse(grepl("^IN[A-Z]", pre_date),
+                                            as.Date(pre_date, format = "%d/%m/%y"),
+                                            as.Date(pre_date, format = "%m/%d/%y")),
+                                     origin = "1970-01-01"))
 
 }
 #________________________________________________________
@@ -129,32 +129,32 @@ load_field_grav <- function(){
 # Load aqe file
 load_field_aqe <- function(){
 
-  return(lapply(list.files("../data/field/aqe",
-                           pattern = "AQE.csv",
-                           full.names = TRUE),
-                function(x) readr::read_csv(x, skip = 1, trim_ws = TRUE,
-                                            col_names = c("tag", "date", "time",
-                                                          "temp_units", "pol_units",
-                                                          "flow_units", "t_amb", "t_stack",
-                                                          "t_preheat", "o2", "co", "co2",
-                                                          "stack_draft", "so2", "velocity",
-                                                          "pressure", "rh", "dew_point",
-                                                          "wet_bulb_temp", "vocs"),
-                                            col_types = cols(
-                                              .default = col_double(),
-                                              tag = col_character(),
-                                              date = col_date(format = "%m/%d/%y"),
-                                              time = col_time(format = "%H:%M:%S"),
-                                              temp_units = col_character(),
-                                              pol_units = col_character(),
-                                              flow_units = col_character()),
-                                            na = c("", "NA", "   NA"))) %>% 
-           dplyr::bind_rows() %>%
-           dplyr::mutate(datetime = as.POSIXct(paste(date, time), 
-                                                     format = "%Y-%m-%d %H:%M:%S")) %>%
-           dplyr::mutate(time = as.numeric(substr(datetime, 12, 13)) * 60 * 60 +
-                                as.numeric(substr(datetime, 15, 16)) * 60 +
-                                as.numeric(substr(datetime, 18, 19))))
+  lapply(list.files("../data/field/aqe",
+                    pattern = "AQE.csv",
+                    full.names = TRUE),
+         function(x) readr::read_csv(x, skip = 1, trim_ws = TRUE,
+                                     col_names = c("tag", "date", "time",
+                                                   "temp_units", "pol_units",
+                                                   "flow_units", "t_amb", "t_stack",
+                                                   "t_preheat", "o2", "co", "co2",
+                                                   "stack_draft", "so2", "velocity",
+                                                   "pressure", "rh", "dew_point",
+                                                   "wet_bulb_temp", "vocs"),
+                                     col_types = cols(
+                                       .default = col_double(),
+                                       tag = col_character(),
+                                       date = col_date(format = "%m/%d/%y"),
+                                       time = col_time(format = "%H:%M:%S"),
+                                       temp_units = col_character(),
+                                       pol_units = col_character(),
+                                       flow_units = col_character()),
+                                     na = c("", "NA", "   NA"))) %>% 
+    dplyr::bind_rows() %>%
+    dplyr::mutate(datetime = as.POSIXct(paste(date, time), 
+                                        format = "%Y-%m-%d %H:%M:%S")) %>%
+    dplyr::mutate(time = as.numeric(substr(datetime, 12, 13)) * 60 * 60 +
+                         as.numeric(substr(datetime, 15, 16)) * 60 +
+                         as.numeric(substr(datetime, 18, 19)))
 
 }
 #________________________________________________________
