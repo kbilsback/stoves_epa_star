15a38a952ff462e7f20c2df6aeb0c533b1b96a57
kbilsback <kbilsbac@rams.colostate.edu>
2017-06-23 13:05:35 -0600

    update plot functions

--- r_scripts/plots.r
+++ r_scripts/plots.r
@@ -7,18 +7,15 @@
 # plot timeseries data (default by qc color)
 # takes a data frame and a column name of variable to plot
 field_timeseries_plot <- function(df, y_var, color_var = "qc", x_var = "datetime", facet_var = "hh_id", y_units = "units") {
-  
-  p <- ggplot(df, aes_string(y = y_var, x = x_var, color = color_var)) +
-         geom_line() +
-         scale_fill_discrete(drop = FALSE) +
-         facet_wrap(~df[[facet_var]], ncol = 1, scales = "free") +
-         theme_minimal() +
-         theme(legend.position = "top") +
-         ylab(paste0(y_var, " (", df[[1, y_units]], ")")) +
-         xlab(x_var)
 
-  # return plot
-  return(p)
+  ggplot(df, aes_string(y = y_var, x = x_var, color = color_var)) +
+    geom_line() +
+    scale_fill_discrete(drop = FALSE) +
+    facet_wrap(~df[[facet_var]], ncol = 1, scales = "free") +
+    theme_minimal() +
+    theme(legend.position = "top") +
+    ylab(paste0(y_var, " (", df[[1, y_units]], ")")) +
+    xlab(x_var)
 
 }
 #________________________________________________________
@@ -28,35 +25,29 @@ field_timeseries_plot <- function(df, y_var, color_var = "qc", x_var = "datetime
 # takes a data frame and a column name of variable to plot
 field_boxplot <- function(df, y_var, fill_var = "qc", x_var = "hh_id", y_units = "units") {
 
-  p <-   ggplot(df, aes_string(y = y_var, x = x_var, fill = fill_var)) +
-          geom_boxplot() +
-          scale_fill_discrete(drop = FALSE) +
-          theme_minimal() +
-          theme(legend.position = "top") +
-          ylab(paste0(y_var, " (", df[[1, y_units]], ")")) +
-          xlab(x_var)
+  ggplot(df, aes_string(y = y_var, x = x_var, fill = fill_var)) +
+    geom_boxplot() +
+    scale_fill_discrete(drop = FALSE) +
+    theme_minimal() +
+    theme(legend.position = "top") +
+    ylab(paste0(y_var, " (", df[[1, y_units]], ")")) +
+    xlab(x_var)
 
-  # return plot
-  return(p)
-  
 }
 #________________________________________________________
 
 #________________________________________________________
 # plot field data
 field_pointplot <- function(df, y_var, color_var = "qc", x_var = "hh_id", y_units = "units") {
-  
-  p <- ggplot(df, aes_string(y = y_var, x = x_var, color = color_var)) +
-        geom_point() +
-        scale_fill_discrete(drop = FALSE) +
-        theme_minimal() +
-        theme(legend.position = "top") +
-        ylab(paste0(y_var, " (", df[[1, y_units]], ")")) +
-        xlab(x_var)
-  
-  # return plot
-  return(p)
-  
+
+  ggplot(df, aes_string(y = y_var, x = x_var, color = color_var)) +
+    geom_point() +
+    scale_fill_discrete(drop = FALSE) +
+    theme_minimal() +
+    theme(legend.position = "top") +
+    ylab(paste0(y_var, " (", df[[1, y_units]], ")")) +
+    xlab(x_var)
+
 }
 #________________________________________________________
 
@@ -64,14 +55,15 @@ field_pointplot <- function(df, y_var, color_var = "qc", x_var = "hh_id", y_unit
 # plot stove fuel combinations tested
 plot_test_list <- function(test_list, x_var = stove_type, y_var = fuel_type,
                            id = hh_id, fill_color = field_site) {
-  
+
   ggplot(test_list, aes_string(y = y_var, x = x_var)) + 
-    geom_tile(colour = "white", width = 0.9, height = 0.9, fill = fill_color) +
+    geom_tile(colour = "white", width = 0.9, height = 0.9,
+              aes_string(fill = fill_color)) +
     scale_fill_discrete(na.value = 'grey95') +
     theme_minimal() +
-    theme(legend.position = "none") +
-    theme(axis.text.x = element_text(angle = 35, hjust = 0.95)) +
-    theme(axis.text = element_text(size = 14)) +
+    theme(axis.text.x = element_text(angle = 35, hjust = 0.95),
+          axis.text = element_text(size = 14),
+          legend.position = "top") +
     geom_text(aes(label = id, size = 8)) +
     xlab("") + ylab("")
 
@@ -82,14 +74,11 @@ plot_test_list <- function(test_list, x_var = stove_type, y_var = fuel_type,
 # plot stove fuel combinations tested
 plot_meta_hist <- function(df, var, x_lab = var, bwidth = 15) {
 
-  p <- ggplot(df, aes_string(var)) +
-        geom_histogram(binwidth = bwidth, stat = "count") +
-        theme_minimal() +
-        theme(legend.position = "top") +
-        xlab(x_lab)
-
-  # return plot
-  return(p)
+  ggplot(df, aes_string(var)) +
+    geom_histogram(binwidth = bwidth, stat = "count") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    xlab(x_lab)
 
 }
 #________________________________________________________
@@ -102,18 +91,15 @@ plot_outliers <- function(df, var, xlab = var) {
                         value_norm = (var - mean(var, na.rm = TRUE)) / sd(var, na.rm = TRUE),
                         outlier = ifelse(is_outlier(var), as.character(hh_id), NA))
 
-  p <- ggplot(data, aes_string(x = var, y = value_norm)) +
-        geom_boxplot() +
-        geom_text(aes(label = outlier), na.rm = TRUE, hjust = -0.3, size = 4) +
-        theme_minimal() +
-        ylab("z score normalized value") +
-        xlab("") +
-        theme(axis.text.x = element_text(angle = 35, hjust = 0.95, size = 30)) +
-        theme(axis.text.y = element_text(size = 30),
-              axis.title=element_text(size=40))
+  ggplot(data, aes_string(x = var, y = value_norm)) +
+    geom_boxplot() +
+    geom_text(aes(label = outlier), na.rm = TRUE, hjust = -0.3, size = 4) +
+    theme_minimal() +
+    ylab("z score normalized value") +
+    xlab("") +
+    theme(axis.text.x = element_text(angle = 35, hjust = 0.95, size = 30)) +
+    theme(axis.text.y = element_text(size = 30),
+          axis.title=element_text(size=40))
 
-  # return plot
-  return(p)
-  
 }
 #________________________________________________________
