5ae6e3de339936b20e562236afcb4e00307a4ca5
kbilsback <kbilsbac@rams.colostate.edu>
2017-06-26 13:48:23 -0600

    fix plotting function

--- r_scripts/plots.r
+++ r_scripts/plots.r
@@ -125,18 +125,30 @@ plot_dodge <- function(df, y_var, y_label, filter_var, x_var = "sample_id",
 #________________________________________________________
 
 #________________________________________________________
-# plot dodge bar chart
+# plot dot plot with geom smooth
 
 plot_dot_line <- function(df, y_var, y_label, filter_var, x_var = "fp",
-                          x_label = "firepower (kW)", facet_1 = "stove", plot_color = "fuel") {
+                          x_label = "firepower (kW)", facet_1 = "stove", plot_color = "sample_id") {
   
   p_df <- df %>%
           dplyr::filter(sample_id != "start_up", sample_id != "shutdown") %>%
-          dplyr::filter(fuel_type == filter_var)
+          dplyr::filter(fuel_type == filter_var) 
+  
+  m <- p_df %>%
+       dplyr::group_by_(facet_1) %>%
+       dplyr::do(model = lm(paste(eval(y_var), "~", eval(x_var)), .)) %>%
+       dplyr::mutate(eqn = get_lm_eqn(model))
 
-  ggplot(p_df, aes_string(x = x_var, y = y_var, color = plot_color)) +
-    geom_point() +
-    geom_smooth(model = "lm") +
+  eqn <- data.frame(eqn = unclass(m$eqn),
+                    stove = m$stove)
+
+  ggplot(p_df, aes_string(x = x_var, y = y_var)) +
+    geom_point(aes_string(color = plot_color), size = 2) +
+    geom_smooth(method = "lm", formula = 'y ~ poly(x,2)',
+                color = 'black') +
+    geom_text(aes(x = -Inf, y = Inf, label = eqn),
+              data = eqn, color = 'black', size = 7,
+              parse = TRUE, vjust = "inward", hjust = "inward") + 
     theme_bw() + 
     facet_wrap(as.formula(paste("~", facet_1)), scales = "free", ncol = 2) +
     ylab(y_label) +
