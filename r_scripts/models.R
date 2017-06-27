bd8bb57d5a0693ad0bf8c3235cdcca3b5a6a2ec9
kbilsback <kbilsbac@rams.colostate.edu>
2017-06-27 09:22:01 -0600

    update functions for modeling data

--- r_scripts/models.R
+++ r_scripts/models.R
@@ -13,7 +13,7 @@ plot_boxcox_model <- function(data, eqn, response) {
 
   data <- data %>%
           dplyr::filter(pol == response) %>%
-          dplyr::group_by(stove)
+          dplyr::group_by(stove_cat)
 
   models <- data %>%
             dplyr::do(model = lm(f, data = .))
@@ -23,11 +23,55 @@ plot_boxcox_model <- function(data, eqn, response) {
              dplyr::mutate(trans = box_cox$x[which.max(box_cox$y)])
   
   data <- data %>%
-          dplyr::left_join(dplyr::select(box_cox, stove, trans), by = "stove")
+          dplyr::left_join(dplyr::select(box_cox, stove_cat, trans), by = "stove_cat")
 
   models_trans <- data %>%
                   dplyr::do(model = lm(f_trans, data = .))
 
+  ggplot(models %>% broom::glance(model),
+         aes(x = stove_cat, y = r.squared, color = 'model')) +
+    geom_point(size = 5) + 
+    geom_point(data = models_trans %>% broom::glance(model),
+               aes(x = stove_cat, y = r.squared, color = 'boxcox transformed model'), size = 5) + 
+    ggtitle(gsub("val", response, eqn)) +
+    theme_bw() + 
+    xlab("stove category") +
+    ylab("R squared") +
+    theme(text = element_text(size = 18), legend.position = "top",
+          axis.text.x = element_text(angle = 45, hjust = 1))
+
+}
+
+#________________________________________________________
+
+#________________________________________________________
+# plot model and boxcox transformation
+
+predict_boxcox_model <- function(train_data, test_data, eqn, response) {
+  
+  f <- formula(eqn)
+  f_trans <- formula(gsub(" ~", "^trans ~", eqn))
+  
+  train_data <- train_data %>%
+                dplyr::filter(pol == response) %>%
+                dplyr::group_by(stove_cat)
+  
+  test_data <- test_data %>%
+               dplyr::filter(pol == response) %>%
+               dplyr::group_by(stove_cat) %>%
+               dplyr::do(pred_val = predict(lm(f, data = train_data),
+                                            test_data, by = groups))
+  
+  box_cox <- data %>% 
+    dplyr::do(box_cox = MASS::boxcox(f, data =., plotit = FALSE)) %>%
+    dplyr::mutate(trans = box_cox$x[which.max(box_cox$y)])
+  
+  data <- data %>%
+    dplyr::left_join(dplyr::select(box_cox, stove, trans), by = "stove")
+  
+  models_trans <- data %>%
+    dplyr::do(model = lm(f_trans, data = .))
+  
   ggplot(models %>% broom::glance(model),
          aes(x = stove, y = r.squared, color = 'model')) +
     geom_point(size = 5) + 
@@ -39,7 +83,7 @@ plot_boxcox_model <- function(data, eqn, response) {
     ylab("R squared") +
     theme(text = element_text(size = 18), legend.position = "top",
           axis.text.x = element_text(angle = 45, hjust = 1))
-
+  
 }
 
 #________________________________________________________
