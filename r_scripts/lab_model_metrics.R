3bd881f1cb70c260afea20e5d03b438b5e2e4e3e
kbilsback <kbilsbac@rams.colostate.edu>
2017-07-10 21:37:25 -0600

    delete function add rmse calculation

--- r_scripts/lab_model_metrics.R
+++ r_scripts/lab_model_metrics.R
@@ -15,6 +15,14 @@ rmse_id_avg <- function(model, lab_data){
 #________________________________________________________
 
 #________________________________________________________
+# Calculate the root mean square error
+calc_rmse <- function(model){
+  mod_resid_sq <- residuals(model)^2
+  return(sqrt(sum(mod_resid_sq)/length(mod_resid_sq)))
+}
+#________________________________________________________
+
+#________________________________________________________
 # Calculate the root mean square error for a model prediction
 rmse_predict <- function(model, lab_data){
 
@@ -23,44 +31,5 @@ rmse_predict <- function(model, lab_data){
   # rmse
   return(sqrt(sum(residuals^2)/length(residuals)))
 
-}
-#________________________________________________________
-
-#________________________________________________________
-# Leave one out basic hr
-leave_one_out <- function(eqn, lab_data) {
-
-  rmse <- lab_data %>%
-          dplyr::group_by(stove, fuel) %>% split(.$stove)
-    do(res = wrapper(.)) 
-          dplyr::do(rmse = leave_one_out_rmse(., stove, fuel))
-
-  # model full
-  mod <- glm(eqn, data = lab_data)
-  rmse$rmse_full = rmse_predict(mod, lab_data)
-  # add id column
-  rmse$id <- ids
-  # return
-  return(rmse)
-
-}
-#_______________________________________________________
-
-#________________________________________________________
-leave_one_out_rmse <- function(lab_data, stove_type, fuel_type){
-
-  # training
-  in_data <- lab_data %>%
-             dplyr::filter(stove != stove_type) %>%
-             dplyr::filter(fuel != fuel_type)
-
-  # removed
-  out_data <- lab_data %>%
-              dplyr::filter(stove == stove_type) %>%
-              dplyr::filter(fuel == fuel_type)
-
-  mod <- glm(eqn, data = in_data)
-
-  rmse <- rmse_predict(mod, out_data)
 }
 #________________________________________________________
