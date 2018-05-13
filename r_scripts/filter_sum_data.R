b9a0cf5fd25e059a884310d255dc8586f9c12c79
ricpie <ricpie@gmail.com>
2018-05-13 09:44:30 +0100

    Added correct grouping approach to the events provided by Danny. They were not being grouped at all, and had many short events.  Now doing the same grouping as the independently sumsarized events.

--- r_scripts/filter_sum_data.R
+++ r_scripts/filter_sum_data.R
@@ -7,13 +7,13 @@ library(dplyr)
 
 #________________________________________________________
 # load field metadata and convert each column to appropriate R class
-filter_sum_data <- function(xx,field_temp_meta,join_duration){
+filter_sum_data <- function(input_data,field_temp_meta,join_duration,cooking_group){
   
 
   
   #For uganda data, find which files are from Uganda, and find the hh_id in the file name.  Then join with the meta data to get the logger id, field_site, and stove_type
   field_temp_meta_uganda <- dplyr::filter(field_temp_meta,grepl("uganda",field_site, fixed=TRUE,ignore.case=TRUE))
-  field_sumsarized_events_uganda <-  dplyr::filter(xx,grepl("Uganda",filename, fixed=TRUE)) %>%
+  field_sumsarized_events_uganda <-  dplyr::filter(input_data,grepl("Uganda",filename, fixed=TRUE)) %>%
     dplyr::mutate(hh_id = substring(filename, 19, 21)) %>%       
     dplyr::left_join(dplyr::select(field_temp_meta_uganda, hh_id, logger_id, field_site, stove,notes,start_date,end_date),
                                     by = "hh_id") %>%
@@ -27,7 +27,7 @@ filter_sum_data <- function(xx,field_temp_meta,join_duration){
   
   #Honduras has no data in the meta data file, it is all in the filename.
   #Need to deal with duplicate loggers on a stove
-  field_sumsarized_events_honduras <-  dplyr::filter(xx,grepl("honduras",filename, fixed=TRUE)) %>%
+  field_sumsarized_events_honduras <-  dplyr::filter(input_data,grepl("honduras",filename, fixed=TRUE)) %>%
     dplyr::mutate(hh_id = substring(filename, 
                                     sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[1])+1, 
                                     sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[2])-1)) %>%
@@ -51,7 +51,7 @@ filter_sum_data <- function(xx,field_temp_meta,join_duration){
   
     
   #Need to deal with duplicate loggers on a stove.            
-  field_sumsarized_events_china <-  dplyr::filter(xx,!grepl("Uganda|India|honduras",filename)) %>%
+  field_sumsarized_events_china <-  dplyr::filter(input_data,!grepl("Uganda|India|honduras",filename)) %>%
     dplyr::mutate(hh_id = substring(filename, 
                                     sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[3])+1, 
                                     sapply(filename, function(x) unlist(gregexpr('_',x,perl=TRUE))[4])-1)) %>%  
@@ -76,7 +76,7 @@ filter_sum_data <- function(xx,field_temp_meta,join_duration){
   
   #Need to deal with duplicate loggers on a stove.  
   field_temp_meta_india <- dplyr::filter(field_temp_meta,grepl("india",field_site, fixed=TRUE))
-  field_sumsarized_events_india <-  dplyr::filter(xx,grepl("India",filename, fixed=TRUE)) %>%
+  field_sumsarized_events_india <-  dplyr::filter(input_data,grepl("India",filename, fixed=TRUE)) %>%
     dplyr::mutate(logger_id = substring(filename, 
                                         sapply(filename, function(x) tail(unlist(gregexpr('_',x,perl=TRUE)),3)[1])+1, 
                                         sapply(filename, function(x) tail(unlist(gregexpr('_',x,perl=TRUE)),3)[2])-1)) %>%
@@ -106,6 +106,73 @@ filter_sum_data <- function(xx,field_temp_meta,join_duration){
                           dplyr::mutate(end_date = as.POSIXct(end_date) )
                                                           
   
-
+  #Group cooking events based on the cooking_group variable.  Must be greater than X minutes, 
+  #into which they were grouped initially by Geocene
+  if (cooking_group>0){
+  cooking_events_timeseries_group <- data.frame(filename=character(),
+                                          start_time=as.POSIXct(character()),
+                                          duration_minutes=as.numeric(),
+                                          hh_id=factor(),
+                                          stove=factor(),
+                                          logger_id=factor(), 
+                                          field_site=factor(),  
+                                          stove_use_category=factor(),
+                                          start_date = as.POSIXct(character()), 
+                                          end_date = as.POSIXct(character()),
+                                          notes=character(),
+                                          event_num=as.numeric(),
+                                          use_flag = as.logical())
+  
+  
+  for (i in unique(field_sumsarized_events_all$file_indices)) {
+    #Grab data from file i, and keep only the entries that are marked as cooking
+    temp <- dplyr::filter(field_sumsarized_events_all,file_indices == i) %>%
+        dplyr::filter(!duplicated(start_time)) 
+    #if any cooking time is found, make an event from it.
+    if (dim(temp)[1]>1) {
+      
+      #If date is NA, give it a good one.
+          if (is.na(as.POSIXct(temp$end_date[1],origin = "1970-01-01 UTC"))) { 
+            start_date_temp = as.POSIXct(min(temp$start_time),origin = "1970-01-01 UTC")
+            end_date_temp = as.POSIXct(max(temp$start_time),origin = "1970-01-01 UTC")
+          } else if (temp$end_date[1] == as.POSIXct('2017-01-01',origin = "1970-01-01 UTC")) { #If end date is 2017-01-01, this was used in the log sheet as code that we did not know the end date.  use the values from the files in this case.
+            start_date_temp = as.POSIXct(min(temp$start_time),origin = "1970-01-01 UTC")
+            end_date_temp = as.POSIXct(max(temp$start_time),origin = "1970-01-01 UTC")
+          } else { #If not NA and not 2017-01-01, must have been found in the meta data, use it.
+            start_date_temp = as.POSIXct(min(temp$start_date),origin = "1970-01-01 UTC")
+            end_date_temp = as.POSIXct(max(temp$end_date),origin = "1970-01-01 UTC")
+          }      
+      
+      #This section is different from the one for the time series because we already have events here, so we don't need to look at the indices,
+      #Rather we just look at the times of starting/stopping directly.
+      #Here we care about the difference between the end of the first cooking event and the start of the next one.
+      event_end_time <- as.numeric(temp$start_time)+temp$duration_minutes*60 #Time of the end of each cooking event in UNIX time (seconds from 1970)
+      event_start_time <- as.numeric(temp$start_time) ##Time of the start of each cooking event in UNIX time
+      #This is the basis for grouping.
+      difftime_events_minutes <- (event_start_time[2:length(event_end_time)] - event_end_time[1:length(event_end_time)-1])/60 #Difference in time between end of first and start of second.  
+      
+      breakstart <- c(0,which((difftime_events_minutes>cooking_group) == TRUE))+1 #Start of a cooking event, by index
+      breakend <- c(which((difftime_events_minutes>cooking_group) == TRUE),dim(temp)[1]) #End of cooking event. 
+      #Last point includes the last point of an actual cooking event. 
+      
+      #Add cooking events to the cooking_events_timeseries_group data frame.
+      cooking_events_timeseries_group <- rbind(cooking_events_timeseries_group,
+                                         data.frame(filename = temp$filename[breakstart],
+                                           start_time= as.POSIXct(temp$start_time[breakstart],origin = "1970-01-01 UTC"),
+                                           duration_minutes=(event_end_time[breakend]-event_start_time[breakstart])/60,
+                                           hh_id=as.factor(temp$hh_id[breakstart]),
+                                           stove=factor(temp$stove[breakstart]),
+                                           logger_id=factor(temp$logger_id[breakstart]),
+                                           field_site=as.factor(temp$field_site[breakstart]),
+                                           stove_use_category=factor(temp$stove_use_category[breakstart]),
+                                           end_date = end_date_temp,
+                                           start_date = start_date_temp,
+                                           notes = temp$notes[1],
+                                           event_num=temp$event_num[breakstart],
+                                           use_flag=as.logical(rep(1,length(breakstart)[1])))) 
+      }
+  }
+  cooking_events_timeseries_group
+} else {field_sumsarized_events_all}
   }
 #________________________________________________________
